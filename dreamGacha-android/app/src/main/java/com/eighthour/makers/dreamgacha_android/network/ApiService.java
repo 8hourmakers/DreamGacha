8400ff58486a46dab16a57179a2ad8fc96465101
mesoibum joon kim <omjoonkim@gmail.com>
2017-07-30 14:35:50 +0900

    chore : add file update example

--- dreamGacha-android/app/src/main/java/com/eighthour/makers/dreamgacha_android/network/ApiService.java
+++ dreamGacha-android/app/src/main/java/com/eighthour/makers/dreamgacha_android/network/ApiService.java
@@ -13,4 +13,19 @@ public interface ApiService {
 
 	@GET
 	Call<String> getStrings();
+
+
+//	@Multipart
+//	@POST( "/api/member/create" )
+//	retrofit2.Call<ResponseRegisterJson> goRegister( @Query( "kind" ) int kind, @Query( "token" ) String token,
+//													 @Query( "email" ) String email,
+//													 @Query( "nickname" ) String nickname,
+//													 @Query( "password" ) String password,
+//													 @Query( "gender" ) String gender,
+//													 @Query( "birthday" ) String birthday,
+//													 @retrofit2.http.Part( "description" ) okhttp3.RequestBody description,
+//													 @retrofit2.http.Part MultipartBody.Part profileImage,
+//													 @Query( "agent" ) String agent,
+//													 @Query( "device_id" ) String device_id
+//	);
 }
--- dreamGacha-android/app/src/main/java/com/eighthour/makers/dreamgacha_android/network/ServerQuery.java
+++ dreamGacha-android/app/src/main/java/com/eighthour/makers/dreamgacha_android/network/ServerQuery.java
@@ -1,6 +1,9 @@
 package com.eighthour.makers.dreamgacha_android.network;
 
 
+import java.io.File;
+
+import okhttp3.MultipartBody;
 import retrofit2.Callback;
 
 
@@ -14,4 +17,21 @@ public class ServerQuery {
 
 		ServiceGenerator.createService( ApiService.class, false ).getStrings().enqueue( callback );
 	}
+
+//	public static void goRegister( int kind, String email, String nickName, String password, String gender, String birthDay, File profileImage, String device_id, retrofit2.Callback callback ) {
+//
+//		okhttp3.RequestBody requestBody = null;
+//		if ( profileImage != null )
+//			requestBody = okhttp3.RequestBody.create( okhttp3.MediaType.parse( "multipart/form-data" ), profileImage );
+//		MultipartBody.Part body =
+//				MultipartBody.Part.createFormData( "profileImage", profileImage.getName(), requestBody );
+//		String descriptionString = "hello, this is description speaking";
+//		okhttp3.RequestBody description =
+//				okhttp3.RequestBody.create(
+//						okhttp3.MediaType.parse( "multipart/form-data" ), descriptionString );
+//
+//		retrofit2.Call<ResponseRegisterJson> call = ServiceGenerator.createService( ServerAPI.class, true ).goRegister( kind, "", email, nickName, password, gender, birthDay, description, body, "A", device_id );
+//
+//		call.enqueue( callback );
+//	}
 }
